98df35d0f8e188a717830446779e12569b8bcd6f
Charles Rodriguez <charlesrodriguez19@gmail.com>
2019-07-08 22:21:29 -0400

    terminado funcionalidad de matriz latina

--- src/matrizlatina/MatrizLatina.java
+++ src/matrizlatina/MatrizLatina.java
@@ -3,21 +3,41 @@ import java.util.Scanner;
 public class MatrizLatina {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int matriz[][] = new int[4][4];
+        int fila = 4, column = 4;
+        int matriz[][] = new int[fila][column];
         
         System.out.println(">> primeros 4 valores que conformarán su matriz latina.");
         for(int i = 0; i < 1; i++){
-            for(int j = 0; j < 4; j++){
+            for(int j = 0; j < column; j++){
                 System.out.print("valor en la posición["+i+"]["+j+"]: ");
                 matriz[i][j] = sc.nextInt();
             }
         }
+        
         System.out.println(">> tus primeros 4 valores fueron:");
         for(int i = 0; i < 1; i++){
-            for(int j = 0; j < 4; j++){
+            for(int j = 0; j < column; j++){
                 System.out.print(matriz[i][j]+" ");
             }
             System.out.println("\n");
         }
+        
+        for(int i = 1; i < fila; i++){
+            for(int j = 0; j < column; j++){
+                if(j != 0) { // saltando cuando j es igual a cero, porque es con otra formula
+                    matriz[i][j] = matriz[i-1][j-1]; //matriz[1][1] = matrtiz[0][0]
+                } else { // J es cero
+                    matriz[i][j] = matriz[i-1][column-1];
+                }
+            }
+        }
+        
+        for(int i = 0; i < fila; i++){
+            for(int j = 0; j < column; j++){
+                System.out.print(matriz[i][j]+" ");
+            }
+            System.out.print("\n");
+        }
+        
     }
 }
